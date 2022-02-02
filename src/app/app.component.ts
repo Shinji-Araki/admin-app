b1be80d4408ec21ff66e471e2fa78f8bc09a9558
test <arakitennis@gmail.com>
2022-02-02 11:04:18 +0900

    社員管理システム完成版

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'admin-app';
+  title = '自社社員名簿';
+
+  constructor(private mainTitle: Title){
+    this.mainTitle.setTitle("社員名簿システム");
+  }
 }
